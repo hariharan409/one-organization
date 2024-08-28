431866a63019580b855e8c3ba9b6c03608a22652
hariharan409 <hharan987@gmail.com>
2024-08-28 10:51:50 +0800

    add steve card design feature

--- src/pages/dashboard/EnterpriseTabView.js
+++ src/pages/dashboard/EnterpriseTabView.js
@@ -1,11 +1,6 @@
 import React from "react";
 import {enterpriseList} from "./external-links/externalAppList";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
 import "../../scss/enterprise-tab-view.scss";
 
 const EnterpriseTabView = () => {
@@ -15,21 +10,15 @@ const EnterpriseTabView = () => {
             {
                 enterpriseList.map((enterprise) => {
                     return(
-                        <Card key={enterprise.id} className="enterprise-tab-view-grid-item">
-                            <CardMedia component="img" sx={{height: "150px",objectFit: "cover"}} src={enterprise.appImage} title="hi" />
-                            <CardContent>
-                                <Typography variant="body1" component="div" sx={{textTransform: "capitalize"}}>
-                                    {enterprise.appName}
-                                </Typography>
-                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                                    {enterprise.appDescription}
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button sx={{padding: "3px",backgroundColor: "primary.main",color: "#FFF",width: "50%"}} size="small">login using seatrium email</Button>
-                                <Button sx={{padding: "3px",backgroundColor: "primary.main",color: "#FFF",width: "50%"}} size="small">don't have seatrium email</Button>
-                            </CardActions>
-                        </Card>
+                        <div key={enterprise.id} className="enterprise-tab-view-grid-item">
+                            <CardMedia component="img" className="enterprise-tab-view-grid-item-image" src={enterprise.appImage} title="hi" />
+                            <div className="enterprise-tab-view-grid-item-use-seatrium-email-button">
+                                <span>login using seatrium email</span>
+                            </div>
+                            <div className="enterprise-tab-view-grid-item-dont-have-seatrium-email-button">
+                                <span>don't have seatrium email</span>
+                            </div>
+                        </div>
                     )
                 })
             }
