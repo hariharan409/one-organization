7530037116965cef7fb1539464ae2c437478acb8
hariharan409 <hharan987@gmail.com>
2024-08-29 09:36:01 +0800

    add slide props

--- src/pages/dashboard/sliderProps.js
+++ src/pages/dashboard/sliderProps.js
@@ -6,35 +6,35 @@ export const sliderProps = (appsLength) => {
         infinite: false,
         speed: 1000,
         slidesToShow: 5,
-        slidesToScroll: Math.min(appsLength,5),
+        // slidesToScroll: Math.max(appsLength,5),
         rows: 2,
         responsive: [
             {
                 breakpoint: 1520, // For screens smaller than 1520px
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: Math.min(appsLength,4),
+                    // slidesToScroll: Math.min(appsLength,4),
                 }
             },
             {
                 breakpoint: 1300, // For screens smaller than 1300px
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: Math.min(appsLength,3),
+                    // slidesToScroll: Math.min(appsLength,3),
                 }
             },
             {
                 breakpoint: 1000, // For screens smaller than 1000px
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: Math.min(appsLength,2),
+                    // slidesToScroll: Math.min(appsLength,2),
                 }
             },
             {
                 breakpoint: 668, // For screens smaller than 668px
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: Math.min(appsLength,1),
+                    // slidesToScroll: Math.min(appsLength,1),
                 }
             }
         ]
