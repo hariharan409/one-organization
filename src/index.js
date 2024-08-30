5e41d34881aed5ca86af1d8f3b5f4a29cb50f084
hariharan409 <hharan987@gmail.com>
2024-08-30 14:05:13 +0800

    update file name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { SearchProvider } from './contexts/SearchContext';
